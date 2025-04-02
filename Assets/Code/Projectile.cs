2ac9ad2fb1c481223cc38d9dc44675aeafcaa19a
byubyuchan <ehdrjs1410@naver.com>
2025-04-03 03:27:30 +0900

    [Feat] 원거리 투사체 기믹 완성
    모든 적을 관통하는 void 투사체 / 한 번에 많은 적을 타격하는 Electricity 투사체 기믹 완성 / C급 투사체 fireball로 변경

--- Assets/Code/Projectile.cs
+++ Assets/Code/Projectile.cs
@@ -10,6 +10,7 @@ public class Projectile : MonoBehaviour
     public Tower tower;
 
     private bool isActive; // 투사체 활성 상태 확인
+    private Vector3 moveDirection;
 
     // 초기화 메서드: 타워와 목표 설정
     public void Init(Tower _tower, GameObject _target)
@@ -17,7 +18,26 @@ public class Projectile : MonoBehaviour
         tower = _tower;
         target = _target;
         isActive = true; // 활성화 상태로 설정
-        StartCoroutine(MoveToTarget());
+
+        if (tower.projectileIndex == 3) // 특수한 프로젝타일인 경우
+        {
+            if (target != null)
+            {
+                moveDirection = (target.transform.position - transform.position).normalized; // 타겟을 향한 초기 방향 설정
+                transform.up = moveDirection; // 투사체의 회전 방향 설정
+            }
+            StartCoroutine(MoveStraight());
+            Invoke("DeactivateProjectile", 10f);
+        }
+        else if (tower.projectileIndex == 4 )
+        {
+            transform.position = target.transform.position;
+            Invoke("DeactivateProjectile", 0.433f);
+        }
+        else // 일반적인 프로젝타일
+        {
+            StartCoroutine(MoveToTarget());
+        }
     }
 
     // 목표를 향해 이동하는 코루틴
@@ -38,12 +58,48 @@ public class Projectile : MonoBehaviour
         DeactivateProjectile();
     }
 
+    IEnumerator MoveStraight()
+    {
+        while (isActive)
+        {
+            transform.position += moveDirection * speed * Time.deltaTime; // 현재 방향으로 이동
+            transform.Rotate(Vector3.forward * 1000 * Time.deltaTime);
+            yield return null;
+        }
+    }
+
     // 충돌 처리 (OnCollisionEnter2D)
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Enemy"))
         {
             HitTarget(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy")) // 적과 충돌했을 때
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(tower.damage); // 적에게 데미지 적용
+            }
+
+            if (tower.projectileIndex == 4)
+            {
+                GameObject effectInstance = GameManager.instance.pool.Get(effectIndex);
+                effectInstance.transform.position = enemy.transform.position;
+                effectInstance.SetActive(true);
+            }
+            else
+            {
+                GameObject effectInstance = GameManager.instance.pool.Get(effectIndex);
+                effectInstance.transform.position = transform.position;
+                effectInstance.SetActive(true);
+            }
+                
         }
     }
 
