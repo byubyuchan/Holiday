e4ad00c2fdca62a2597f844b36f7f3268a3bb791
wind283 <rlaehgud300@gmail.com>
2025-07-26 05:45:40 +0900

    [Feat] 해상도 설정 구현
    에디터에서 말고 빌드 후 테스트를 해봐야 제대로 확인할 수 있으나 빌드가 계속 안 돼서 확인은 못 해봄.

---
+++ Assets/Code/ResolutionManager.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResolutionManager : MonoBehaviour
+{
+    FullScreenMode screenMode;
+    public Dropdown resolutionDropdown;
+    public Toggle fullScreenButton;
+    List<Resolution> resolutions = new List<Resolution>();
+    public int resolutionNum;
+
+    private void Start()
+    {
+        InitUI();
+    }
+
+    void InitUI()
+    {
+        resolutions.AddRange(Screen.resolutions);
+        resolutionDropdown.options.Clear();
+
+        int optionNum = 0;
+        foreach (Resolution item in resolutions)
+        {
+            Dropdown.OptionData option = new Dropdown.OptionData();
+            option.text = item.width + " x " + item.height;
+            resolutionDropdown.options.Add(option);
+
+            if(item.width == Screen.width && item.height == Screen.height)
+            {
+                resolutionDropdown.value = optionNum;
+            }
+            optionNum++;
+        }
+        resolutionDropdown.RefreshShownValue();
+
+        fullScreenButton.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow) ? true : false;
+    }
+
+    public void DropboxOptionChange(int x)
+    {
+        resolutionNum = x;
+    }
+
+    public void FullScreenButton(bool isFullScreen)
+    {
+        screenMode = isFullScreen ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
+    }
+
+    public void OkButtonClick()
+    {
+        Screen.SetResolution(resolutions[resolutionNum].width,
+            resolutions[resolutionNum].height,
+            screenMode);
+    }
+}
