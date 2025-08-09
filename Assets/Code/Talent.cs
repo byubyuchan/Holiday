16a44831195a938ad669f52851c84e8abf4a92b3
wind283 <rlaehgud300@gmail.com>
2025-08-09 17:55:33 +0900

    [Feat] 특성 1번 구현
    talent.cs에 특성을 추가하는 방식으로 구현하였습니다.

--- Assets/Code/Talent.cs
+++ Assets/Code/Talent.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 public class Talent : MonoBehaviour
 {
     RectTransform rect;
+    Transform[] talents;
 
     void Awake()
     {
         rect = GetComponent<RectTransform>();
+        Transform talentGroup = transform.Find("Panel/TalentGroup");
+        if (talentGroup != null)
+        {
+            talents = new Transform[talentGroup.childCount];
+            for (int i = 0; i < talentGroup.childCount; i++)
+            {
+                talents[i] = talentGroup.GetChild(i);
+            }
+        }
     }
 
     public void Show()
@@ -17,5 +27,10 @@ public class Talent : MonoBehaviour
     public void Hide()
     {
         rect.localScale = Vector3.zero;
+    }
+
+    public void Talent1()
+    {
+        GameManager.instance.Gold += Random.Range(0, 101);
     }
 }
