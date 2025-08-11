50dea0f0c24d371c3d5aeeff55f9efcced48acdc
wind283 <rlaehgud300@gmail.com>
2025-08-11 14:29:12 +0900

    [Feat] 특성 선택 랜덤화
    라운드가 끝난 후 뜨는 특성 창에서 현재 만든 1~7번까지의 특성에서 랜덤한 3개 중 하나를 고를 수 있도록 하였습니다.
    
    맨 아래에는 특성을 선택하지 않고 넘어가는 버튼을 넣었습니다.

--- Assets/Code/Talent.cs
+++ Assets/Code/Talent.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class Talent : MonoBehaviour
 {
     RectTransform rect;
     Transform[] talents;
+
+    [SerializeField]
+    Transform talentGroup;
+
+    [SerializeField]
+    RectTransform layoutRoot;
+
+    HashSet<int> picked = new HashSet<int>();
+
     private int RandomGold;
     [SerializeField]
     DataBaseConnectingTest dbConnector;
@@ -11,19 +22,19 @@ public class Talent : MonoBehaviour
     void Awake()
     {
         rect = GetComponent<RectTransform>();
-        Transform talentGroup = transform.Find("Panel/TalentGroup");
-        if (talentGroup != null)
-        {
-            talents = new Transform[talentGroup.childCount];
-            for (int i = 0; i < talentGroup.childCount; i++)
-            {
-                talents[i] = talentGroup.GetChild(i);
-            }
-        }
+
+        if (talentGroup == null)
+            talentGroup = transform.Find("Panel/Talent Group");
+
+        int childCount = talentGroup.childCount;
+        talents = new Transform[childCount];
+        for (int i = 0; i < childCount; i++)
+            talents[i] = talentGroup.GetChild(i);
     }
 
     public void Show()
     {
+        Select();
         rect.localScale = Vector3.one;
     }
 
@@ -32,18 +43,53 @@ public class Talent : MonoBehaviour
         rect.localScale = Vector3.zero;
     }
 
+    void Select()
+    {
+        if (talents == null || talents.Length < 2) return;
+
+        foreach (var t in talents) t.gameObject.SetActive(false);
+
+        List<int> candidates = new List<int>();
+        for (int i = 1; i < talents.Length; i++)
+            if (!picked.Contains(i)) candidates.Add(i);
+
+        for (int i = 0; i < candidates.Count; i++)
+        {
+            int j = Random.Range(i, candidates.Count);
+            (candidates[i], candidates[j]) = (candidates[j], candidates[i]);
+        }
+
+        int toPick = Mathf.Min(3, candidates.Count);
+        for (int k = 0; k < toPick; k++)
+            talents[candidates[k]].gameObject.SetActive(true);
+
+        var t0 = talents[0];
+        t0.gameObject.SetActive(true);
+        t0.SetSiblingIndex(t0.parent.childCount - 1);
+
+        var root = layoutRoot != null ? layoutRoot : (RectTransform)talentGroup;
+        LayoutRebuilder.ForceRebuildLayoutImmediate(root);
+    }
+
+    void OnPicked(int idx)
+    {
+        if (idx > 0) picked.Add(idx);
+    }
+
     public void Talent1()
     {
         RandomGold = Random.Range(0, 101);
         GameManager.instance.Gold += RandomGold;
         GameManager.instance.ShowMessage($"{RandomGold}만큼 골드를 획득했습니다!");
         dbConnector.saveTalent($"Talent1_{RandomGold}만큼 골드 획득");
+        OnPicked(1);
     }
 
     public void Talent2()
     {
         TowerManager.instance.sale = true;
         dbConnector.saveTalent("선택하지 않습니다.");
+        OnPicked(2);
     }
 
     public void Talent3()
@@ -51,6 +97,7 @@ public class Talent : MonoBehaviour
         TowerMaker.instance.RandomPay = 4;
         TowerMaker.instance.selectedTowerPrefab = null;
         dbConnector.saveTalent("랜덤가격이 4가됩니다.");
+        OnPicked(3);
     }
 
     public void Talent4()
@@ -58,6 +105,7 @@ public class Talent : MonoBehaviour
         TowerMaker.instance.MeleePay = 6;
         TowerMaker.instance.selectedTowerPrefab = null;
         dbConnector.saveTalent("전사의 가격이 6이됩니다.");
+        OnPicked(4);
     }
 
     public void Talent5()
@@ -65,6 +113,7 @@ public class Talent : MonoBehaviour
         TowerMaker.instance.RangedPay = 6;
         TowerMaker.instance.selectedTowerPrefab = null;
         dbConnector.saveTalent("마법사의 가격이 6이됩니다.");
+        OnPicked(5);
     }
 
     public void Talent6()
@@ -72,6 +121,7 @@ public class Talent : MonoBehaviour
         TowerMaker.instance.TankPay = 6;
         TowerMaker.instance.selectedTowerPrefab = null;
         dbConnector.saveTalent("전위 가격이 6이 됩니다.");
+        OnPicked(6);
     }
 
     public void Talent7()
@@ -81,5 +131,6 @@ public class Talent : MonoBehaviour
         TowerMaker.instance.selectedTowerPrefab = null;
         GameManager.instance.ShowMessage($"랜덤 타워의 가격이 {RandomGold}G로 변경되었습니다!");
         dbConnector.saveTalent($"'랜덤의 랜덤_랜덤' 타워의 가격이 {RandomGold}G로 변경되었습니다!");
+        OnPicked(7);
     }
 }
